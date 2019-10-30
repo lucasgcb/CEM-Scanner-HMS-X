ff072bb1721521a8ef623ae49aae60f517af52da
= <=>
2019-10-30 18:27:30 -0300

    Add table view, preview, cell centerer

--- analisador_placa/analisador_placa/analisador.py
+++ analisador_placa/analisador_placa/analisador.py
@@ -4,7 +4,8 @@ Created on Tue Aug 27 15:18:45 2019
 
 @author: Dave
 """
-
+import time
+import datetime
 
 class Interrupt_Error(Exception):
         pass
@@ -14,10 +15,7 @@ def instrument_clean_up(instrument,interface,detector,error_msg=" ",):
     interface.label_status.setText("Esperando")
     interface.label_erro.setText(error_msg)
     detector.release()
-
-def read_level_at_freq(instrument,freq):
-    instrument.write('RMOD:FREQ {}'.format(freq))
-    return instrument.receiver_level
+    interface_scan_finished(interface)
 
 def interrupt_instrument(instrument,interface):
     interface.label_status.setText("Esperando")
@@ -25,40 +23,61 @@ def interrupt_instrument(instrument,interface):
     instrument.close()
     raise Interrupt_Error
 
-def scan(interface,running,interrupt,semafaro,detector):
+def interface_scan_start(interface):
+    interface.botao_vai.setEnabled(False)
+    interface.botao_parar.setEnabled(True)
+    interface.botao_instrumento.setEnabled(False)
+    interface.box_nome.setEnabled(False)
+    interface.label_erro.setText(" ")
+    interface.label_status.setText("Iniciando...")
+
+def instrument_setup(Instrument,freq,interface):
+    instrument = Instrument(interface.box_instrumento.currentText())
+    instrument.activate_mode_receiver()
+    instrument.write('RMOD:FREQ {}'.format(freq))
+    time.sleep(0.5)
+    interface.label_status.setText("Escaneando...")
+    return instrument
+
+def interface_scan_finished(interface):
+    interface.botao_vai.setEnabled(True)
+    interface.botao_parar.setEnabled(False)
+    interface.botao_instrumento.setEnabled(True)
+    interface.box_nome.setEnabled(True)
+
+def dim_gen(indiceX,char):
+    return [char for y in range(0,indiceX)]
+
+def scan(interface,running,interrupt,semafaro,detector,model):
     from scpiinterface import Instrument
     import csv
     unidades = {"kHz": 1000,
                 "MHz": 1000000,
                 "GHz": 1000000000}
+                
     while(True):
         semafaro.acquire()
+        interface_scan_start(interface)
         if running.is_set() is False:
             return
-        interface.botao_vai.setEnabled(False)
-        interface.botao_parar.setEnabled(True)
-        interface.botao_instrumento.setEnabled(False)
-        interface.box_nome.setEnabled(False)
-        interface.label_status.setText("Escaneando...")
-        interface.label_erro.setText(" ")
         
-        print("eaemen")
         unidade = unidades[str(interface.box_frequencia_unidade.currentText())]
         freq = interface.box_frequencia.value() * unidade
         indiceX = interface.box_dimensao_X.value()
         indiceY = interface.box_dimensao_Y.value()
-   
+        
         try:
             fname= interface.box_nome.text()
+            
             with open('{}.csv'.format(fname), 'w', newline='') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
                 header = [chr(x+65) for x in range(0,indiceX)]
+                data_list = [dim_gen(indiceX,'?') for Y in range(0,indiceY)]
+                model.update_header(header)
+                model.update_data(data_list)
                 spamwriter.writerow(header)
-                
-                instrument = Instrument(interface.box_instrumento.currentText())
-                instrument.activate_mode_receiver()
+                instrument = instrument_setup(Instrument,freq,interface)
                 print("total:{}".format(indiceX*indiceY))
-                progress = []
                 for i in range(0,indiceY):
                     #interface.label_colunas.setText(" ".join(header))
                     row = []
@@ -70,14 +89,14 @@ def scan(interface,running,interrupt,semafaro,detector):
                         if running.is_set() is False:
                             return
                         if interrupt.is_set() is False:
-                            val = read_level_at_freq(instrument,freq)
-                            progress.append(str(val))
+                            val = instrument.receiver_level
                             row.append(val)
+                            data_list[i][j] = val
+                            model.update_data(data_list)
                             #interface.label_linhas.setText(" ".join(progress))
                         else:
                             interrupt_instrument(instrument,interface)  
                     spamwriter.writerow(row)
-                    progress.append('\n')
 
             print(instrument.hello)
             instrument_clean_up(instrument,interface,detector)
@@ -90,7 +109,6 @@ def scan(interface,running,interrupt,semafaro,detector):
                         return
                 instrument_clean_up(instrument,interface,detector, "Erro: Interrompido.")
             except Exception:
-                interface.label_status.setText("Esperando")
                 interface.label_erro.setText("Erro: Cheque Conexão.")
                 if running.is_set() is False:
                             return
@@ -102,11 +120,8 @@ def scan(interface,running,interrupt,semafaro,detector):
                         return
                 instrument_clean_up(instrument,interface,detector, "Erro: Desconhecido. Cheque conexão.")
             except Exception:
-                interface.label_status.setText("Esperando")
                 interface.label_erro.setText("Erro: Cheque Conexão.")
                 if running.is_set() is False:
                             return
-        interface.botao_vai.setEnabled(True)
-        interface.botao_parar.setEnabled(False)
-        interface.botao_instrumento.setEnabled(True)
-        interface.box_nome.setEnabled(True)
+       
+        interface.box_nome.setText("arquivo_" + datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S"))
